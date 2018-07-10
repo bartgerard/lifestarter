8895e6eff451b8c7c08d5aced491840e623648cd
bartgerard <bart.gerard@gmail.com>
2018-07-10 18:05:55 +0200

    First

---
+++ service/src/main/java/be/gerard/lifestarter/LifestarterApplication.java
@@ -0,0 +1,12 @@
+package be.gerard.lifestarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifestarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifestarterApplication.class, args);
+	}
+}
