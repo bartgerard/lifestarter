02b753f0c16cb7d5a3b690790048e713d2a8ad0a
bartgerard <bart.gerard@gmail.com>
2019-02-23 16:37:40 +0100

    v0.1.3 + Polishing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import {RouterModule, Routes} from '@angular/router';
 import {ProjectComponent} from './component/project/project.component';
 import {RegistrationComponent} from './component/registration/registration.component';
 import {InfoComponent} from './component/info/info.component';
+import {UpdatesComponent} from './component/updates/updates.component';
 
 const routes: Routes = [
   {path: '', component: InfoComponent},
+  {path: 'updates', component: UpdatesComponent},
   {path: 'project', component: ProjectComponent},
   {path: 'registration', component: RegistrationComponent}
 ];
